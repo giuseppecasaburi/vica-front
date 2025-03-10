ca05809169f337bd75bdf406e9d206da2f51f0f3
giuseppecasaburi <enzobiondi0000@libero.it>
2025-03-10 22:09:41 +0100

    Rimuovi i pulsanti in favore di link diretti nella sezione eroe, semplifica il codice CSS e migliora la responsività

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -16,20 +16,16 @@ function HeroSection() {
                 {/* Contenuto sopra l'overlay */}
                 <div className="hero-content">
                     <h1 className="fw-bold animate-title">🏡✨ Trasforma il tuo bagno in un’oasi di design ✨🛁</h1>
-                    <p id="hero-text" className="fs-5 animate-text">
+                    <p id="hero-text" className="animate-text">
                         Scopri la nostra selezione di arredi bagno eleganti e funzionali. <br />Dai lavabo moderni alle docce di lusso, ogni dettaglio è pensato per offrirti comfort e stile.
                     </p>
                     <div className="hero-buttons animate-text">
-                        <button className="btn btn-color-p">
-                            <Link to={"/collezioni"}>
-                                Esplora la nostra collezione
-                            </Link>
-                        </button>
-                        <button className="btn btn-color-s ">
-                            <Link to={"/contatti"}>
-                                Richiedi un preventivo gratuito
-                            </Link>
-                        </button>
+                        <Link to={"/collezioni"} className="btn btn-color-p" aria-label="Esplora la collezione di arredo bagno">
+                            Esplora la nostra collezione
+                        </Link>
+                        <Link to={"/contatti"} className="btn btn-color-s" aria-label="Richiedi un preventivo gratuito">
+                            Richiedi un preventivo gratuito
+                        </Link>
                     </div>
                 </div>
             </div>
